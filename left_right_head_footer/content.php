9767dd850e727e313da3ed03bbda3a8cf59d9cc1
sanket sanjay jadhav <111295449+python1605@users.noreply.github.com>
2022-12-27 17:05:33 +0530

    Add files via upload

---
+++ left_right_head_footer/content.php
@@ -0,0 +1,37 @@
+
+<div id="layoutSidenav_content">
+ <div class="img">      
+          <img src="https://www.zeiss.com.my/content/dam/z/vis-b2b/reference-master/images/support/business-support/support_benefit_01.jpg" width="100%" height="597">
+                <div class="center">
+                    <h1><b>  Find <br>Your<br><b> Business</b><br> Partner</b> </h1>
+                </div>
+                <div class="logo3">
+                  <img src="img/logo3.png" width="220" height="220">
+                </div>  
+                <div class="qoute">
+                    <h5> “The way to get started is to quit talking and start doing.” <br> -Walt Disney </h5>
+                </div>
+ </div>
+ <footer class="py-4 bg-light mt-auto">
+        <div class="container-fluid px-4">
+            <div class="d-flex align-items-center justify-content-between small">
+                <div class="text-muted">Copyright &copy; Your Website 2022</div>
+                <div>
+                    <a href="#">Privacy Policy</a>
+                    &middot;
+                    <a href="#">Terms &amp; Conditions</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="assets/demo/chart-area-demo.js"></script>
+<script src="assets/demo/chart-bar-demo.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+<script src="js/datatables-simple-demo.js"></script>
+</body>
+</html>
---
+++ left_right_head_footer/left_panel.php
@@ -0,0 +1,43 @@
+<div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+            <div class="sb-sidenav-menu">
+                <div class="nav">
+                    <a class="nav-link" href="index.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                        Dashboard
+                    </a>
+                    <div class="sb-sidenav-menu-heading">Manage Pages</div>
+                    <a class="nav-link" href="registration.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                        Registration
+                    </a>
+                    <a class="nav-link" href="contact_us.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                        Contact Us
+                    </a>
+                    <a class="nav-link" href="investor.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                        Investor Profile
+                    </a>
+                    <a class="nav-link" href="partner.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                        Partner Profile
+                    </a>
+                    <a class="nav-link" href="create_blog.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                        Create Blog
+                    </a>
+                    <a class="nav-link" href="index.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+                        Blog
+                    </a>
+
+                </div>
+            </div>
+            <div class="sb-sidenav-footer">
+                <div class="small">Logged in as:</div>
+                Admin
+            </div>
+        </nav>
+    </div>
